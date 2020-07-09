342a84a8eb0b04f2402b5ef1e9621521e0575231
Nick Taylor <nick@dev.to>
2020-07-09 18:41:25 -0400

    Added preact/devtools to all webpack entry points that need it when in production mode.

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,5 +3,21 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 const environment = require('./environment');
+const config = environment.toWebpackConfig();
 
-module.exports = environment.toWebpackConfig();
+// Inject the preact/devtools import into all the webpacker pack files (webpack entry points) that reference at least one Preact component
+// so that Preact compoonents can be debugged with the Preact DevTools.
+config.entry = Object.entries(config.entry).reduce(
+  (previous, [entryPointName, entryPointFileName]) => {
+    if (/\.jsx$/.test(entryPointFileName)) {
+      previous[entryPointName] = ['preact/devtools', entryPointFileName];
+    } else {
+      previous[entryPointName] = entryPointFileName;
+    }
+
+    return previous;
+  },
+  {},
+);
+
+module.exports = config;
